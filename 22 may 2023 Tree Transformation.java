6de7e745c481b2b4054f942ff9451efd835e0e0b
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-05-22 12:13:33 +0530

    Create 22 may 2023 Tree Transformation.java

---
+++ 22 may 2023 Tree Transformation.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] p = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.solve(N, p);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int solve(int N, int[] p) {
+        return Arrays.stream(p).filter(c -> c > 0).boxed().collect(java.util.stream.Collectors.toSet()).size();
+    }
+}
