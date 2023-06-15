76455d37ce046f47ab9a73e4f81977553fb6fb5b
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-06-15 22:59:19 +0530

    Create 15 june 2023 Longest Palindrome in a String.java

---
+++ 15 june 2023 Longest Palindrome in a String.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.longestPalin(S));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static String longestPalin(String s){
+       if (s == null || s.length() < 2) {
+            return s;
+        }
+        int start = 0;
+        int maxLength = 1;
+        for (int i = 0; i < s.length(); i++) {
+           int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > maxLength) {
+                maxLength = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+        return s.substring(start, start + maxLength);
+    }
+    
+    private static int expandAroundCenter(String s, int left, int right) {
+        int L = left;
+        int R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
