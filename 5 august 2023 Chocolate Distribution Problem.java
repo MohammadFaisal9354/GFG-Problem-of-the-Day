492d6eeba0aa2b5d81e849921bef4018c6f70137
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-08-05 11:51:55 +0530

    Create 5 august 2023 Chocolate Distribution Problem.java

---
+++ 5 august 2023 Chocolate Distribution Problem.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int i = 0;i<n;i++)
+                {
+                    int x = sc.nextInt();
+                    arr.add(x);
+                }
+            int m = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+    		System.out.println(ob.findMinDiff(arr,n,m));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        int i=0,j=0;
+        Collections.sort(a);
+        long mini=(long)1e9;
+        while(j<n){
+            int len=j-i+1;
+            if(len==m) mini=Math.min(mini,a.get(j)-a.get(i));
+            if(len>m) i++;
+            else j++;
+        }
+        return mini;
+    }
+}
