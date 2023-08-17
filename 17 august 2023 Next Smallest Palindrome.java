0e218732a91dd875979641c75f2719a576469500
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-08-17 11:49:21 +0530

    Create 17 august 2023 Next Smallest Palindrome.java

---
+++ 17 august 2023 Next Smallest Palindrome.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int num[] = new int[n];
+            String[] str = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                num[i] = Integer.parseInt(str[i]);
+            }
+
+            Vector<Integer> ans = new Solution().generateNextPalindrome(num, n);
+            for (Integer x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    Vector<Integer> generateNextPalindrome(int num[], int n) {
+        Vector<Integer> v = new Vector<>();
+        if(isnine(num)){
+            for(int i=0;i<n+1;i++){
+                if(i==0||i==n) v.add(1);
+                else v.add(0);
+            }
+            return v;
+        }
+        boolean flag=isRight(num,n);
+        int i=(n-1)/2;
+        int j=n/2;
+        while(i>=0){
+            if(flag){
+                if(num[i]==9){
+                    num[i]=0;
+                }else{
+                    num[i]++;
+                    flag=false;
+                }
+            }
+            num[j++]=num[i--];
+        }
+        for(i=0;i<n;i++)
+            v.add(num[i]);
+        return v;
+    }
+    boolean isRight(int[] num,int n){
+        int i=(n-1)/2;
+        int j=n/2;
+        
+        while(i>=0&&j<n){
+            if(num[i]<num[j]) return true; 
+            else if(num[i]>num[j]) return false;
+            
+            i--;
+            j++;
+        }
+        return true;
+    }
+    boolean isnine(int[] num){
+        for(int i=0;i<num.length;i++)
+            if(num[i]!=9) return false;
+        return true;
+    }
+}
