14c8467775db04ab01981bdb513c0c9e7e1ff48a
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-08-29 12:48:04 +0530

    Create 29 august 2023 Delete nodes having greater value on right.java

---
+++ 29 august 2023 Delete nodes having greater value on right.java
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head)
+    {
+        Node temp=head;
+        Node ptr=null;
+        Node prev=temp;
+        while(temp.next!=null){
+        ptr=temp.next;
+        while(ptr!=null && !(ptr.data>temp.data ) )
+            ptr=ptr.next;
+        if(ptr!=null)
+        {
+            if(temp==head)
+            {
+                head=head.next;
+                prev=temp=head;
+                continue;
+            }
+           else
+           prev.next=temp.next;
+        }
+       else
+       prev=temp;
+       temp=temp.next;
+     }
+     return head;
+    }
+}
+  
