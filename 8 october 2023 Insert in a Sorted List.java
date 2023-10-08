a3b36bf1626cfc429217f6b7363c098f8c7b75e8
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-10-08 09:00:48 +0530

    Create 8 october 2023 Insert in a Sorted List.java

---
+++ 8 october 2023 Insert in a Sorted List.java
@@ -0,0 +1,24 @@
+class Solution {
+    Node sortedInsert(Node head, int key) {
+        Node nn=new Node(key);
+        if(head==null)
+        return nn;
+        Node temp=head;
+        Node prev=null;
+        while(temp!=null && temp.data<key){
+            prev=temp;
+            temp=temp.next;
+        }
+        if(temp==null)
+            prev.next=nn;
+        else if(prev==null){
+            nn.next=head;
+            head=nn;
+        }
+        else{
+            nn.next=prev.next;
+            prev.next=nn;
+        }
+        return head;
+    }
+}
