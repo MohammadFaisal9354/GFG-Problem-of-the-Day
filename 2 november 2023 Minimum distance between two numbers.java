2a061ace1d8818af3732ad9888c9c01a40641dc8
Mohammad Faisal <faisalkhan9354@gmail.com>
2023-11-02 06:44:42 +0530

    Create 2 november 2023 Minimum distance between two numbers.java

---
+++ 2 november 2023 Minimum distance between two numbers.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) 
+                a[i] = Integer.parseInt(str[i]);
+            String[] xy = br.readLine().trim().split(" ");
+            int x = Integer.parseInt(xy[0]);
+            int y = Integer.parseInt(xy[1]);
+            Solution g = new Solution();
+            System.out.println(g.minDist(a, n, x, y));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minDist(int a[], int n, int x, int y) {
+        int xi = -1;
+        int yi = -1;
+        int minDist = -1;
+        for(int i=0; i<n; ++i) {
+            if(a[i] == x) {
+                xi = i;
+                if(yi > -1 && (minDist == -1 || minDist > Math.abs(yi-xi))) 
+                    minDist = Math.abs(yi-xi);
+            }
+            if(a[i] == y) {
+                yi = i;
+                if(xi > -1 && (minDist == -1 || minDist > Math.abs(yi-xi))) 
+                    minDist = Math.abs(yi-xi);
+            }
+        }
+        return minDist;
+    }
+}
