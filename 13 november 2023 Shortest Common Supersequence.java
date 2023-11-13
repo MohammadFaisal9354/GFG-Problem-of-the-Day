f2a039afd46ef46e26db294bb7d65aa36f6ca63a
Mohammad Faisal <faisalkhan9354@gmail.com>
2023-11-13 07:55:27 +0530

    Create 13 november 2023 Shortest Common Supersequence.java

---
+++ 13 november 2023 Shortest Common Supersequence.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int t=sc.nextInt();
+		
+		sc.nextLine();
+		while(t-->0)
+		{
+		   //taking String X and Y
+		   String S[]=sc.nextLine().split(" ");
+		   String X=S[0];
+		   String Y=S[1];
+		   
+		   //calling function shortestCommonSupersequence()
+		   System.out.println(new Solution().shortestCommonSupersequence(X, Y, X.length(), Y.length()));
+		}
+	}
+
+
+
+
+       
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find length of shortest common supersequence of two strings.
+    public static int shortestCommonSupersequence(String X,String Y,int m,int n)
+    {
+        int[] L = new int[n + 1];
+        for (int i = 1; i <= m; i++) {
+            int prev = 0;
+            for (int j = 1; j <= n; j++) {
+                int current = L[j];
+                 if (X.charAt(i- 1) == Y.charAt(j- 1))
+                    L[j] = prev + 1;
+                else
+                    L[j] = Math.max(L[j], L[j - 1]);
+                prev = current;
+            }
+        }
+        return m+n-L[n];
+    }
+}
