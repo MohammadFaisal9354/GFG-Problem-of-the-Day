3d94967fd0b401f7ce1a04f9010a0387a9c6773f
Mohammad Faisal <faisalkhan9354@gmail.com>
2023-12-11 10:29:12 +0530

    Create 11 december 2023 Max Sum Subarray of size K.java

---
+++ 11 december 2023 Max Sum Subarray of size K.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N=sc.nextInt();
+            int K=sc.nextInt();
+            ArrayList<Integer> Arr = new ArrayList<Integer>(N); 
+  
+            // Appending new elements at 
+            // the end of the list 
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+                Arr.add(x); 
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.maximumSumSubarray(K,Arr,N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+        long s=0,a=0l;
+        for(int i=0;i<K;i++) s+=Arr.get(i);
+        a=Math.max(a,s);
+        int i=0;
+        for(int j=K;j<N;j++){
+            s-=Arr.get(i++);
+            s+=Arr.get(j);
+            a=Math.max(a,s);
+        }
+        return a;
+    }
+}
