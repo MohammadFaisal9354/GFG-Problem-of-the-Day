cf43c7f6a0af8a512f11c08b7b4d9fe1146ac646
Mohammad Faisal <faisalkhan9354@gmail.com>
2024-01-09 21:48:11 +0530

    Create 9 January 2024 Search Pattern (KMP-Algorithm).java

---
+++ 9 January 2024 Search Pattern (KMP-Algorithm).java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s, patt;
+            s = sc.next();
+            patt = sc.next();
+            
+            Solution ob = new Solution();
+            
+            ArrayList<Integer> res = ob.search(patt, s);
+            if(res.size()==0)
+                System.out.print(-1);
+            else {
+                for(int i = 0;i<res.size();i++)
+                    System.out.print(res.get(i) + " ");
+            }
+            System.out.println();    
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    ArrayList<Integer> search(String pat, String txt) {
+        ArrayList<Integer> indices = new ArrayList<>();
+        int M = pat.length();
+        int N = txt.length();
+        for (int i = 0; i <= N - M; i++) {
+            int j;
+            for (j = 0; j < M; j++) {
+                if (txt.charAt(i + j) != pat.charAt(j)) 
+                    break;
+            }
+            if (j == M) 
+                indices.add(i + 1);
+        }
+        return indices;
+    }
+}
