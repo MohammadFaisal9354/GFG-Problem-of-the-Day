ae474a6bc833b8707c1acc1f081c8e902e3018bc
Mohammad Faisal <faisalkhan9354@gmail.com>
2024-02-26 11:00:35 +0530

    Create 26 February 2024 Power Set.java

---
+++ 26 February 2024 Power Set.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine().trim();
+            Solution ob = new Solution();
+            List<String> ans = ob.AllPossibleStrings(s);
+            for(String i: ans)
+                System.out.print(i + " ");
+            System.out.println();
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+  public List<String> AllPossibleStrings(String s) {
+        List<String> result = new ArrayList<>();
+        generateSubsequences(s, "", result);
+        Collections.sort(result);
+        return result;
+    }
+
+    private void generateSubsequences(String original, String current, List<String> result) {
+        if (!current.equals(""))
+            result.add(current);
+        for (int i = 0; i < original.length(); i++) {
+            generateSubsequences(original.substring(i + 1), current + original.charAt(i), result);
+        }
+    }
+}
